1d4bb2e78c797a5655ae93231ae17ad51e7e72d4
Edige <edige-k@mail.ru>
2023-06-30 13:28:05 +0600

    second commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -110,9 +110,11 @@
         /*---------------------------------------------------
             counter
         ----------------------------------------------------*/
-        $('.counter-single h2').counterUp({
-            delay: 10, // the delay time in ms
-            time: 1000 // the speed time in ms
+        $(document).ready(function() {
+            $('.counter-single h2').counterUp({
+                delay: 10,
+                time: 1000
+            });
         });
 
         /*---------------------------------------------------
